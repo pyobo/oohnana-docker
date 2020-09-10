a4068a95495e6963c4b2d0c69bfa02a66d32d1be
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-09-10 11:52:03 +0200

    Fix tags

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 # Exit on first command that goes wrong
 set -e
 
-TMPIMAGENAME=obo_postgres4
+HASH=$(git rev-parse HEAD)
+TMPIMAGENAME="oohnana_${HASH:0:10}"
 IMAGENAME=oohnana
 TAG="1.0.0"
 
@@ -13,12 +14,12 @@ docker container run -e POSTGRES_PASSWORD=pyobo -p 5433:5432 -t -d --name $TMPIM
 python -m pip install -q pyobo
 python -m pyobo.database.sql.loader --uri postgresql+psycopg2://postgres:pyobo@localhost:5433/postgres
 
-docker container commit $TMPIMAGENAME $IMAGENAME:TAG
+docker container commit $TMPIMAGENAME $IMAGENAME:$TAG
 docker container stop $TMPIMAGENAME
 
 # VERIFY, run on a different port to be sure
 # docker container run -p 5436:5432 --rm -it --name $IMAGENAME $IMAGENAME
 
 docker login
-docker tag $IMAGENAME:TAG cthoyt/$IMAGENAME:TAG
-docker push cthoyt/$IMAGENAME:TAG
+docker tag $IMAGENAME:TAG cthoyt/$IMAGENAME:$TAG
+docker push cthoyt/$IMAGENAME:$TAG
